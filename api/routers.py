636b693540d3bddcf737d0fdb34b7c608a805ffe
sasriawesome <sasri.project@gmail.com>
2022-06-19 05:17:44 +0700

    add user router #4

--- api/routers.py
+++ api/routers.py
@@ -1,6 +1,8 @@
+from djoser.views import UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register("users", UserViewSet, basename="user")
 
 app_name = "v1"
 
